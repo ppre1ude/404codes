9061109ebedfc27a1061802bbb32d3d1ac20b3f9
ppre1ude <95406268+ppre1ude@users.noreply.github.com>
2024-09-30 09:20:47 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int w;
+	double h, a; // width, height, area
+
+	cin >> w >> h;
+
+	a = w * h / 2;
+	printf("%.1f", a);
+
+	return 0;
+
+}
