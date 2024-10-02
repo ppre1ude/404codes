2aca194b07df331b83db2da4ae98dca11bf4b448
ppre1ude <95406268+ppre1ude@users.noreply.github.com>
2024-10-02 09:17:51 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	int a1, b1, c1, d1;
+	a1 = 56 * a;
+	b1 = 24 * b;
+	c1 = 14 * c;
+	d1 = 6 * d;
+
+	cout << a1 + b1 + c1 + d1;
+	return 0;
+
+}
