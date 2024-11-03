a30d17b2e2ff44c86f931cc092c2c0a91b23df98
ppre1ude <95406268+ppre1ude@users.noreply.github.com>
2024-11-03 21:37:57 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 124 ms, Memory: 20868 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <cctype>
+using namespace std;
+
+void solve(void) {
+  int n, m;
+  cin >> n >> m;
+
+  map<string, int> a;
+  vector<string> b(n+1);
+    
+  for (int i=1; i<=n; i++) {
+    cin >> b[i];
+    a[b[i]] = i;
+  }
+
+  string s;
+    
+  for (int i=0; i<m; i++) {
+    cin >> s;
+    if (isdigit(s[0])) cout << b[stoi(s)];
+    else cout << a[s];
+    cout << "\n";
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
